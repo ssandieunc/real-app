94fba9bb4e08ba61ae88cefb3d4438fc42896193
u329702 <scott.sandie@unchealth.unc.edu>
2022-10-19 10:34:21 -0400

    latest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'EventList' }">Events</router-link> |
+      <router-link :to="{ name: 'AboutView' }">About</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style>
@@ -14,17 +16,17 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
+#nav {
   padding: 30px;
 }
-
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
-nav a.router-link-exact-active {
+#nav a.router-link-exact-active {
   color: #42b983;
+}
+h4 {
+  font-size: 20px;
 }
 </style>
